83e7961b3888a62a4c63cf82467105b400422efb
Paolo Raez <str237@gmail.com>
2022-10-04 09:43:35 +0200

    Move styles into src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Welcome } from '../src/components/welcome'
+import styles from '@/styles/Home.module.css'
+import { Welcome } from '@/components/welcome'
 
 export default function Home() {
   return (
